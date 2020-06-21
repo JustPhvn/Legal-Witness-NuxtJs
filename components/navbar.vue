91788a359ddba33bc40f1e6bb4c7dc772ab5b984
JustPhvn <justicequyphan@gmail.com>
2020-06-21 15:06:48 -0700

    init commit

---
+++ components/navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand to="/">Legal Expert Witness</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav> </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="/about">About</b-nav-item>
+            <b-nav-item-dropdown text="Witnesses" right>
+              <b-dropdown-item to="/wone">W. One</b-dropdown-item>
+              <b-dropdown-item>W. Two</b-dropdown-item>
+              <b-dropdown-item>W. Three</b-dropdown-item>
+              <b-dropdown-item>W. Four</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style>
+.navbar {
+  background-color: #0f0f0f !important;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-container>
+      <b-jumbotron>
+        <template v-slot:header>Legal Expert Witness</template>
+
+        <template v-slot:lead>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ut
+          quibusdam ratione esse maiores cum nisi odio? Accusantium ad provident
+          porro illum magnam aliquid modi eos placeat quaerat mollitia. Minima?
+        </template>
+
+        <hr class="my-4" />
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni labore
+          voluptates alias natus voluptatem dolore ipsum, placeat ea numquam.
+          Maxime consectetur nostrum in dolorum similique sit, mollitia fuga
+          suscipit magni?
+        </p>
+
+        <b-button v-b-modal.modal-1>Contact</b-button>
+
+        <b-modal id="modal-1" title="Contact">
+          <p class="my-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            minima architecto consequuntur neque culpa tempora reiciendis,
+            distinctio numquam aperiam adipisci eaque et nobis non saepe iusto
+            cumque. Mollitia, nemo nostrum?
+          </p>
+        </b-modal>
+      </b-jumbotron>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
